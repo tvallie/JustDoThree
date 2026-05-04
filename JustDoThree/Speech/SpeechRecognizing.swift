257398fe830e5184986b6817bcfbd3525d7a1072
Todd Vallie <todd@vallie.com>
2026-05-04 19:05:15 -0400

    feat: add SpeechRecognizing protocol and dictation state types

---
+++ JustDoThree/Speech/SpeechRecognizing.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+enum SpeechAuthState: Equatable {
+    case notDetermined
+    case authorized
+    case denied
+    case restricted
+}
+
+enum DictationState: Equatable {
+    case idle
+    case requestingAuth
+    case recording
+    case error(String)
+}
+
+protocol SpeechRecognizing: AnyObject {
+    var isAvailable: Bool { get }
+    var supportsOnDevice: Bool { get }
+
+    func requestAuthorization(_ completion: @escaping (SpeechAuthState) -> Void)
+
+    func start(
+        requireOnDevice: Bool,
+        onPartial: @escaping (String) -> Void,
+        onFinish: @escaping () -> Void,
+        onError: @escaping (Error) -> Void
+    )
+
+    func stop()
+}
